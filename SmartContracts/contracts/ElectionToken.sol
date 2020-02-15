493d14843543b4096cd715af417269b53b76e04a
Dylan Mooers <dmooers@calpoly.edu>
2020-02-15 13:25:50 -0800

    Changed token address to voter

--- SmartContracts/contracts/ElectionToken.sol
+++ SmartContracts/contracts/ElectionToken.sol
@@ -2,11 +2,30 @@ pragma solidity ^0.5.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721Pausable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721Burnable.sol";
 
-contract ElectionToken is ERC721Full, ERC721Pausable {
 
+contract ElectionToken is ERC721Full, ERC721Pausable, ERC721Burnable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    uint256 
 
-    constructor(string ElectionName, string abrev) public {
-        ERC721Full(ElectionName, abrev);
+    constructor(string memory _electionName, string memory _electionAbrev)
+    ERC721Full(_electionName, _electionAbrev) public {
+    }
+
+    // Creates a 721 token for the creator of the election
+    // Uses the election name to fill the token URI, in order to
+    // Authenticate a real voter
+    function mintVote(address voter, string memory electionName)
+        public returns (unit256) {
+        
+        _tokenIds.increment();
+
+        uint256 voteID = _tokenIds.current();
+        _mint(voter, tokenID);
+        _setTokenURI(voteID, electionName);
+
+        return voteID;
     }
 }
